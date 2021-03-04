6eb7273370a0831e7b49e382f259f8b051041adc
sevcator <68725282+sevcator@users.noreply.github.com>
2021-03-04 19:45:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,25 @@
-echo Начало декоратив Termux'а!
 
-echo Подготовка Установка tsu
+
+
 pkg install tsu -y
-echo  
-echo 1) Удаление MOTD
-echo  
+
+
+
 cd ..
 cd usr/etc
 rm -rf motd
-cd ..
-cd ..
+cd ../..
+
 cd home
 apt update -y
 apt upgrade -y
 
-echo 2) Установка фона и шрифта
+
 
 apt install curl -y
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
 
-echo 3) Установка баннера
+
 
 apt install git -y
 git clone https://github.com/Bhai4You/Termux-Banner
