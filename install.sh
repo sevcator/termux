8664636f781c1c74e22471db674fae1adfac9f7f
sevcator <68725282+sevcator@users.noreply.github.com>
2021-03-05 08:17:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,23 @@ sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/instal
 
 apt install git -y
 
+
+
+
+
+
+apt install vim -y
+
+apt install neovim -y
+
+apt install tmux -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pip3 install db0mb3r -U
+
 git clone https://github.com/Bhai4You/Termux-Banner
 
 cd Termux-Banner
@@ -35,15 +52,3 @@ chmod +x t-ban.sh
 bash t-ban.sh
 
 nano 84nn3r.txt && cd ../..
-
-apt install vim -y
-
-apt install neovim -y
-
-apt install tmux -y
-
-pkg install python -y
-
-pkg install python2 -y
-
-pip3 install db0mb3r -U
